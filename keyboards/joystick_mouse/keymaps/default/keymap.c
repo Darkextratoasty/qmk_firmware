4c387b3fb400f786e1301a1ea4cfd88f38e9c706
darkextratoasty <joshuaeblackburn@gmail.com>
2023-10-20 09:57:58 -0400

    nothing impactful

---
+++ keyboards/joystick_mouse/keymaps/default/keymap.c
@@ -0,0 +1,18 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+#include "analog.h"
+#include "pointing_device.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┐
+     * │ A │
+     * └───┘
+     */
+    [0] = LAYOUT_ortho_1x1(
+        KC_A
+    )
+};
+
